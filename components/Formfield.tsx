0fe21e1c020dc4b92660bd77eb02bec07fa1c82d
Paradon Meeanan <112736862+rizbikit1781@users.noreply.github.com>
2025-03-26 15:55:15 -0600

    Implemented form component

---
+++ components/Formfield.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const Formfield = ({ form } : {form : any}) => (
+    <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                        <FormItem>
+                        <FormLabel>Username</FormLabel>
+                        <FormControl>
+                            <Input placeholder="shadcn" {...field} />
+                        </FormControl>
+                        <FormDescription>
+                            This is your public display name.
+                        </FormDescription>
+                        <FormMessage />
+                        </FormItem>
+                    )}
+                    />
+)
+
+export default Formfield
