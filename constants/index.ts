c93203491ff746e2002b879c761f3452458fac4d
Paradon Meeanan <112736862+rizbikit1781@users.noreply.github.com>
2025-04-10 17:00:04 -0600

    Added feedback creation

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,5 @@
 import { CreateAssistantDTO } from "@vapi-ai/web/dist/api";
+//TypeScript-first schema validation with static type inference
 import { z } from "zod";
 
 export const mappings = {
--- lib/actions/general.action.ts
+++ lib/actions/general.action.ts
@@ -1,4 +1,7 @@
+import { feedbackSchema } from "@/constants";
 import { db } from "@/firebase/admin";
+import { google } from "@ai-sdk/google";
+import { generateObject } from "ai";
 
 
 export async function getInterviewByUserId(userId: string): Promise<Interview[] | null> {
@@ -43,9 +46,53 @@ export async function getInterviewById(id: string): Promise<Interview | null> {
 }
 
 
-export async function getFeedbackById() {
-    
+export async function createFeedback(params: CreateFeedbackParams) {
+     
+    const { interviewId, userId, transcript } = params;
+
+    try {
+        const formattedTranscript = transcript
+        .map(( sentence: { role: string; content: string}) => (
+            `- ${sentence.role}: ${sentence.content}\n`
+        )).join('');
+
+        const { object: { totalScore, categoryScores, strengths, areasForImprovement, finalAssessment } } = await generateObject({
+            model: google('gemini-2.0-flash-001', {
+                structuredOutputs: false, 
+            }),
+            schema: feedbackSchema,
+            prompt: `You are an AI interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories. Be thorough and detailed in your analysis. Don't be lenient with the candidate. If there are mistakes or areas for improvement, point them out.
+                    Transcript: ${formattedTranscript}
+                    Please score the candidate from 0 to 100 in the following areas. Do not add categories other than the ones provided:
+                    - **Communication Skills**: Clarity, articulation, structured responses.
+                    - **Technical Knowledge**: Understanding of key concepts for the role.
+                    - **Problem-Solving**: Ability to analyze problems and propose solutions.
+                    - **Cultural & Role Fit**: Alignment with company values and job role.
+                    - **Confidence & Clarity**: Confidence in responses, engagement, and clarity.
+                    `,
+            system:  "You are a professional interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories",
+        })
+
+        const feedback = await db.collection('feedback').add({
+            interviewId,
+            userId,
+            totalScore,
+            categoryScores,
+            strengths,
+            areasForImprovement,
+            finalAssessment,
+            createdAt: new Date().toDateString()
+        })
+
+        return {
+            success: true,
+            feedbackId: feedback.id,
+        }
+    } catch (error) {
+        console.error('Error saving feedback', error)
+    }
     const feedback = await db.collection('feedback')
 
     return  null
 }
+
